f7c65bb178f97b5a45f899dd6be7dc753db39526
Yangzhedi <uiryzd@163.com>
2016-07-14 02:18:15 +0800

    add 寻找素数

--- 300~/344. Reverse String.js
+++ 300~/344. Reverse String.js
@@ -8,3 +8,21 @@ var reverseString = function(s) {
     arr.reverse();
     return arr.join("");
 };
+
+
+//打印素数
+function su(n){
+	var num=0;
+	for(var i=2; i<=n; i++){
+		j=2;
+		while(i%j!=0){
+			j++;
+		}
+		if(j==i){
+			console.log(i);
+			num++;
+		}
+	}
+	console.log(num);
+}
+su(100)
